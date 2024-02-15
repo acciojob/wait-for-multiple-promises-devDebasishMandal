e22ec4817676be39c02802533b76e1da51c83a40
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-15 12:44:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,54 +1,73 @@
-const res = document.getElementById("output");
 
-const promises = [
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 1", time: time / 1000 }), time);
-  }),
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 2", time: time / 1000 }), time);
-  }),
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 3", time: time / 1000 }), time);
-  }),
-];
+let tableBody = document.getElementById("output");
 
-async function callFns() {
-  const start = new Date();
-  // Use Promise.all to wait for all Promises to resolve
-  res.innerHTML += `
-            <tr id="loading">
+tableBody.innerHTML = `
+	<tr id="loading">
                 <td colspan=2>Loading...</td>
             </tr>
-          `;
-await Promise.all(promises)
-    .then((results) => {
-      res.innerHTML = ``;
-      // Log the array of results
-      results.forEach((e) => {
-        res.innerHTML += `
-            <tr>
-                <td>${e.name}</td>
-                <td>${e.time}</td>
+			<tr id="loading">
+                <td colspan=2>Loading...</td>
             </tr>
-          `;
+			<tr id="loading">
+                <td colspan=2>Loading...</td>
+            </tr>
+			`;
+
+let randomTime1 = (Math.floor(Math.random() * 3) + 1) * 1000;
+let randomTime2 = (Math.floor(Math.random() * 3) + 1) * 1000;
+let randomTime3 = (Math.floor(Math.random() * 3) + 1) * 1000;
+
+const P1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res({
+      name: "P1resolved",
+      time: randomTime1 / 1000,
+    });
+  }, randomTime1);
+});
+const P2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res({
+      name: "P2resolved",
+      time: randomTime2 / 1000,
+    });
+  }, randomTime2);
+});
+const P3 = new Promise((res, rej) => {
+  setTimeout(() => {
+    res({
+      name: "P3resolved",
+      time: randomTime3 / 1000,
+    });
+  }, randomTime3);
+});
+
+async function promresolv() {
+  const startTime = new Date();
+
+  Promise.all([P1, P2, P3])
+    .then((data) => {
+      tableBody.innerHTML = ``;
+      // logging all the array
+      data.forEach((e) => {
+        tableBody.innerHTML += `<tr>
+	<td>${e.name}</td>
+	<td>${e.time}</td>
+</tr>`;
       });
+	  const endTime = new Date();
+  const finalTime = endTime - startTime;
+  tableBody.innerHTML += `<tr>
+<td>Total</td>
+<td>${finalTime / 1000}</td>
+</tr>`;
     })
-    .catch((error) => {
-      console.error(error);
+    .catch((err) => {
+      console.log(err);
     });
 
-  const end = new Date();
+  
 
-  const timeInMillis = end - start;
-  res.innerHTML += `
-            <tr>
-                <td>Total</td>
-                <td>${timeInMillis / 1000}</td>
-            </tr>
-          `;
 }
 
-callFns();
+promresolv();
